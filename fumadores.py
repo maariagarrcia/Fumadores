bb7bd55c14fb5d9caa55c5c84b61ef1746571b57
maariagarrcia <mrrmn@myuax.com>
2023-05-01 12:14:24 +0200

    status

--- fumadores.py
+++ fumadores.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process, Lock
 import random
+from enum import Enum
 
 class ProviderModel(Process):
     def __init__(self):
@@ -8,12 +9,33 @@ class ProviderModel(Process):
     def run(self):
         pass
 
+class SmokerStatus(Enum):
+    WAITING_FOR_PAPER = 0
+    WAITING_FOR_TOBACCO = 1
+    WAITING_FOR_MATCHES = 2
+    WAITING_FOR_GREEN= 3
+    WAITING_FOR_FILTER = 4
+    SMOKING = 5
+
+class Ingredient(Enum):
+    PAPER = 0
+    TOBACCO = 1
+    MATCHES = 2
+    GREEN = 3
+    FILTER = 4
+
 class SmokerModel(Process):
-    def __init__(self):
+    def __init__(self, ingredient:Ingredient):
         super(SmokerModel, self).__init__()
 
+        self.my_ingredient:Ingredient = ingredient
+
     def run(self):
-        pass
+        while True:
+            #  conseguir los ingredientes
+
+            # si se consiguen todos los ingredientes
+            pass
 
 class Controller():
     def __init__(self):
